c5dd029ac85c693280acaba48fb52d233dbe8103
dungx <dangvudung18110263@gmail.com>
2020-11-11 15:59:51 +0700

    pagination

--- Book_Shop/Controllers/ProductsController.cs
+++ Book_Shop/Controllers/ProductsController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Book_Shop.Models;
+using PagedList;
 
 namespace Book_Shop.Controllers
 {
@@ -16,9 +17,29 @@ namespace Book_Shop.Controllers
         private Book_StoreEntities2 db = new Book_StoreEntities2();
 
         // GET: Products
-        public ActionResult Index()
+        public ActionResult Index(int? page)
         {
-            return View(db.Products.ToList());
+
+            // 1. Tham số int? dùng để thể hiện null và kiểu int
+            // page có thể có giá trị là null và kiểu int.
+
+            // 2. Nếu page = null thì đặt lại là 1.
+            if (page == null) page = 1;
+
+            // 3. Tạo truy vấn, lưu ý phải sắp xếp theo trường nào đó, ví dụ OrderBy
+            // theo LinkID mới có thể phân trang.
+            var links = (from l in db.Products
+                         select l).OrderBy(x => x.id);
+
+            // 4. Tạo kích thước trang (pageSize) hay là số Link hiển thị trên 1 trang
+            int pageSize = 3;
+
+            // 4.1 Toán tử ?? trong C# mô tả nếu page khác null thì lấy giá trị page, còn
+            // nếu page = null thì lấy giá trị 1 cho biến pageNumber.
+            int pageNumber = (page ?? 1);
+
+            // 5. Trả về các Link được phân trang theo kích thước và số trang.
+            return View(links.ToPagedList(pageNumber, pageSize));
         }
 
         // GET: Products/Details/5
