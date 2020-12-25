fd2c131eb4037a75c2087945b541bbf6c436d317
LQuangTien <tienkg5554@gmail.com>
2020-12-25 13:31:12 +0700

    add chartjs

--- Book_Shop/Controllers/PromoCodesController.cs
+++ Book_Shop/Controllers/PromoCodesController.cs
@@ -1,4 +1,5 @@
 ﻿using Book_Shop.Models;
+using PagedList;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
@@ -11,9 +12,28 @@ namespace Book_Shop.Controllers
         private Book_StoreEntities2 db = new Book_StoreEntities2();
 
         // GET: PromoCodes
-        public ActionResult Index()
+        public ActionResult Index(int? page)
         {
-            return View(db.PromoCodes.ToList());
+            // 1. Tham số int? dùng để thể hiện null và kiểu int
+            // page có thể có giá trị là null và kiểu int.
+
+            // 2. Nếu page = null thì đặt lại là 1.
+            if (page == null) page = 1;
+
+            // 3. Tạo truy vấn, lưu ý phải sắp xếp theo trường nào đó, ví dụ OrderBy
+            // theo LinkID mới có thể phân trang.
+            var links = (from l in db.PromoCodes
+                         select l).OrderBy(x => x.id);
+
+            // 4. Tạo kích thước trang (pageSize) hay là số Link hiển thị trên 1 trang
+            int pageSize = 3;
+
+            // 4.1 Toán tử ?? trong C# mô tả nếu page khác null thì lấy giá trị page, còn
+            // nếu page = null thì lấy giá trị 1 cho biến pageNumber.
+            int pageNumber = (page ?? 1);
+
+            // 5. Trả về các Link được phân trang theo kích thước và số trang.
+            return View(links.ToPagedList(pageNumber, pageSize));
         }
 
         // GET: PromoCodes/Details/5
